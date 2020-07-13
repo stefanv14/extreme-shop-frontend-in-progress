9fd710e91c23aff4959e01354c5a4acb09f6151b
= <vasilijevic032@gmail.com>
2020-07-13 13:58:16 +0200

    Updates

---
+++ precache-manifest.abccc2e9676cdd3ec2b07bbec05b9e1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7864ea912f704bb9fa09a3e621a5474b",
+    "url": "/extreme-shop-frontend-in-progress/index.html"
+  },
+  {
+    "revision": "be3c8e05b3baeb43db7f",
+    "url": "/extreme-shop-frontend-in-progress/static/css/2.72869010.chunk.css"
+  },
+  {
+    "revision": "5109be1fc7f3372360ef",
+    "url": "/extreme-shop-frontend-in-progress/static/css/main.f69f0995.chunk.css"
+  },
+  {
+    "revision": "be3c8e05b3baeb43db7f",
+    "url": "/extreme-shop-frontend-in-progress/static/js/2.1cc268a5.chunk.js"
+  },
+  {
+    "revision": "14060d5403b5cb599a71704f7b391b06",
+    "url": "/extreme-shop-frontend-in-progress/static/js/2.1cc268a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5109be1fc7f3372360ef",
+    "url": "/extreme-shop-frontend-in-progress/static/js/main.2b31c8c6.chunk.js"
+  },
+  {
+    "revision": "7d2d469374847ca22a07",
+    "url": "/extreme-shop-frontend-in-progress/static/js/runtime-main.65f37a3b.js"
+  }
+]);
